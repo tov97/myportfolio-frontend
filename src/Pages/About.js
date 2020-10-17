698df60dc4672319170c30d48eed28cb040333f3
Trevor Tovsen <trevor.tovsen@codedank.com>
2020-10-17 13:44:24 -0500

    added another skill

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import frontend from "../Assets/frontend.png";
 import coding from "../Assets/coding.png";
 import graduation from "../Assets/graduation.png";
 import design from "../Assets/design.png";
@@ -18,7 +19,7 @@ export default function About() {
       title: "Front End Development",
       desc:
         "Proficiency building responsive, dynamic web apps with React JS (w/ Redux, styled-components, React boostrap, and many other libraries). Excellent understanding of HTML, CSS, and Javascript.",
-      image: coding,
+      image: frontend,
     },
     {
       title: "UI/UX Design",
@@ -31,6 +32,12 @@ export default function About() {
       desc:
         "Tested interpersonal skills and experience organizing and promoting products at conferences and trade shows as well as presenting product demonstrations to an audience.",
       image: promotion,
+    },
+    {
+      title: "Other Coding Skills and Certifications",
+      desc:
+        "Completion of the full React JS (60+ hours) and MERN stack courses on Udemy. Also, completion of high level college courses in SQL, Java, R, and practice with the SDLC process.",
+      image: coding,
     },
   ];
   return (
@@ -64,4 +71,10 @@ const CardContainer = styled.div`
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 3em;
   margin: 0% 0% 3% 3%;
+  @media (max-width: 1080px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 640px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
